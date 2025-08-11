4a2b401204a29d80c7a1646267c99e2c41fb6bb4
CharmanderFactory <charlieseghir@gmail.com>
2025-08-10 21:18:22 -0400

    ch. 2 complete

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
-from stats import word_count
+from stats import word_count, char_count
 
 def main():
     file_contents = get_book_text("books/frankenstein.txt")
     num_words = word_count(file_contents)
-    print(f"{num_words} words found in the document") 
+    chars = char_count(file_contents)
+    print(f"{num_words} words found in the document", chars) 
      
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,16 @@ def word_count(file_contents):
     words = file_contents.split()
     num_words = len(words)
     return num_words
+
+def char_count(file_contents):
+    erase_dupes = file_contents.lower()
+    chars = {}
+    for char in erase_dupes:
+        
+        if char not in chars:
+            chars[char] = 1
+        else:
+            chars[char] += 1
+            
+            
+    return chars
