df7746305ab56a3071ff2507a4683766a89685f9
CharmanderFactory <charlieseghir@gmail.com>
2025-08-10 20:32:27 -0400

    refactor

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
+from stats import word_count
+
 def main():
-    print(get_book_text("books/frankenstein.txt")) 
+    file_contents = get_book_text("books/frankenstein.txt")
+    num_words = word_count(file_contents)
+    print(f"{num_words} words found in the document") 
      
 
 main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def word_count(file_contents):
+    words = file_contents.split()
+    num_words = len(words)
+    return num_words
